6633b4f18f2f8a975fdf1f7ea3cd62646e8f17a9
Amir Hossein <vfebhebrb@gmail.com>
2026-09-03 23:38:42 +0330

    create ROBO admin panel

--- config.js
+++ config.js
@@ -3,19 +3,21 @@
 // =====================================
 
 // // حالت تست روی سیستم خودت
-// const API_URL = 
-//     window.location.hostname === "localhost" ||
-//     window.location.hostname === "127.0.0.1"
+const API_URL = 
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
 
-//     ?
+    ?
 
-//     "http://localhost:3000/api"
+    "http://localhost:3000/api"
 
-//     :
+    :
 
-//    "https://iran-go4q.onrender.com/api";
-const API =
-"http://localhost:3000/api";
+   "https://iran-go4q.onrender.com/api";
+
+   
+// const API =
+// "http://localhost:3000/api";
 // بعد از آنلاین شدن بک‌اند Render فقط همین خط تغییر می‌کند
 // مثال:
 // const API_URL = "https://your-backend.onrender.com";
