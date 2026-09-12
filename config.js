5acea1980f6e915d89492b5ab5ef7192fa9813b7
Amir Hossein <vfebhebrb@gmail.com>
2026-09-12 21:57:18 +0330

    update project

--- config.js
+++ config.js
@@ -3,21 +3,29 @@
 // =====================================
 
 // // حالت تست روی سیستم خودت
-const API_URL = 
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
+// const API_URL = 
+//     window.location.hostname === "localhost" ||
+//     window.location.hostname === "127.0.0.1"
 
-    ?
+//     ?
 
-    "http://localhost:3000/api"
+//     "http://localhost:3000/api"
 
-    :
+//     :
 
-   "https://iran-go4q.onrender.com/api";
+//    "https://iran-go4q.onrender.com/api";
 
    
-// const API =
-// "http://localhost:3000/api";
+
+
+// =====================================
+// API CONFIG
+// =====================================
+
+const API_URL =
+   "https://iran-go4q.onrender.com/api";
+
+
 // بعد از آنلاین شدن بک‌اند Render فقط همین خط تغییر می‌کند
 // مثال:
 // const API_URL = "https://your-backend.onrender.com";
